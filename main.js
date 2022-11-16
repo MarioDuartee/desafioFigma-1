e917fcba7688419b055a1852f78c57f85e1bd4d2
Mario Duarte <marioduartee@hotmail.com>
2022-11-16 17:49:20 -0300

    pagina dinamica com objeto2

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 const conteudo = [
     {
-        date: '02 de jul, 2021',
+        date: '02 de jul, 2022',
         title: 'Agora é oficial: o Windows 11 está vindo',
         content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis.'
     },
     {
-        date: '02 de jul, 2021',
+        date: '01 de jul, 2022',
         title: 'Tim Berners-Lee vai leiloar código-fonte da web',
         content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis. Praesent et auctor justo. Vestibulum nisl orci, lacinia venenatis leo sit amet, pulvinar tincidunt risus. Phasellus nisl dui, cursus a lectus et, interdum ullamcorper libero.'
     },
     {
-        date: '02 de jul, 2021',
+        date: '30 de jun, 2022',
         title: 'Tem Firefox novo no pedaço e você vai querer migrar',
         content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est.'
     },
     {
-        date: '02 de jul, 2021',
+        date: '29 de jun, 2021',
         title: 'John McAfee, criador do antivírus McAfee, morre',
         content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis. Praesent et auctor justo. Vestibulum nisl orci, lacinia venenatis leo sit amet, pulvinar tincidunt risus. Phasellus nisl dui, cursus a lectus et, interdum ullamcorper libero.'
     }
